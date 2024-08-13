b7ad3f164a6102b70f08ae0b0d090f8d2e2e95cb
Popsik-max <almazrahmeev@icloud.com>
2024-08-13 23:48:24 +0300

    Update Слишком древний шифр.py

--- Слишком древний шифр.py
+++ Слишком древний шифр.py
@@ -1,12 +1,10 @@
-Pass_1 = int(input('Число первой вставки в промежутке от 3 до 20: ' ))
+number = int(input('Число первой вставки в промежутке от 3 до 20: ' ))
 
-for num_1 in range (1, 21):
-    for num_2 in range (2, 21):
-        num_3 = num_1 + num_2
-        num_4 = Pass_1 / num_3
-        if num_4 - int(num_4) == 0:
-            print('Второй пароль: ', num_1, num_2)
-
-
+for i in range (1, number):
+    for j in range (i+1,number):
+        if number % (i + j) == 0:
+            password = (str(i) + str(j))
+            print(password,end='')
+            continue
         else:
             continue
