de2998bfb25ecb8930c48d109d57e6d94811eea1
Popsik-max <almazrahmeev@icloud.com>
2024-09-04 19:56:20 +0300

    Update class object.py

--- class object.py
+++ class object.py
@@ -1,20 +1,3 @@
-# class Human:
-#     def __init__(self, name, age):
-#         self.name = name
-#         self.age = age
-#         self.say_info()
-#
-#     def say_info(self):
-#         print(f'Привет, меня зовут {self.name}, мне {self.age}')
-#
-#     def bir (self):
-#         self.age += 1
-#         print(f'У меня теперь день рожденья, мне теперь {self.age}')
-
-# den = Human('Денис',22)
-# max = Human("Макс",22)
-# max.bir()
-
 class House():
     def __init__(self,name, number_of_floor):
         self.name = name
@@ -28,9 +11,17 @@ class House():
             for i in range(1, new_floor+1):
                 print(i)
 
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
-h1.go_to(5)
-h2.go_to(10)
+    def __len__(self):
+        return self.number_of_floor
 
+    def __str__(self):
+        return (f'Название:{self.name}, количество этажей: {self.number_of_floor}')
 
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1.__str__())
+print(h2.__str__())
+
+print(h1.__len__())
+print(h2.__len__())
