5c731a15304417977f9b9f3a1c00e6029e5d5341
Popsik-max <almazrahmeev@icloud.com>
2024-09-22 20:14:38 +0300

    Add files via upload

---
+++ homework str and bytes.py
@@ -0,0 +1,38 @@
+from pprint import pprint
+class Shop():
+    def __init__(self,__file_name = 'products.txt'):
+        self.__file_name = 'products.txt'
+
+    def get_products(self):
+        file = open(self.__file_name,'r')
+        return pprint(file.read())
+        file.close()
+
+
+    def add(self,*products):
+        self.products = products
+        for i in self.products:
+            if i.name in self.__file_name:
+                print(f'Продукт {i} уже есть в магазине')
+            else:
+                file1 = open(self.__file_name, 'a')
+                file1.write(f'{i}\n')
+                file1.close()
+
+class Product():
+    def __init__(self, name,weight,category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return (f'{self.name}, {self.weight}, {self.category}')
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2.__str__())
+s1.add(p1, p2, p3)
+print(s1.get_products())
