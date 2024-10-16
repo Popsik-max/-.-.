ea3a49638eede6af74dde4cf267ff022f0304329
Popsik-max <almazrahmeev@icloud.com>
2024-10-16 18:39:19 +0300

    Update Potok.py

--- Potok.py
+++ Potok.py
@@ -6,7 +6,7 @@ time_start = datetime.now()
 def write_words(word_count,file_name):
     with open (file_name, 'a', encoding = 'utf8' ) as file:   #открываем файл для добавления в него строк
         for i in range(word_count):                           #   вносим количество строк которое получим в качестве аргумента
-            wr_file = file.write(f'Какое то слово № {i} \n')          #   переменная представляет из себя запись в файл строки и номера
+            file.write(f'Какое то слово № {i} \n')
             time.sleep(0.1)                                      # задержка 0,1 млск
 
     print(f'Завершилась запись в файл {file_name}')
@@ -23,19 +23,20 @@ print(time_)
 from  threading import Thread    # импортируем базу для создания потока
 import requests  #
 time_start1 = datetime.now()
-first = Thread(target=write_words(10,'example5txt')) # создаем обьект класса потоки, где таргет функция необходиая к выполнению
-second = Thread(target=write_words(30,'example6txt'))
-third = Thread(target=write_words(200,'example7txt'))
-four = Thread(target=write_words(100,'example8txt'))
+first = Thread(target=write_words, args = (10,'example5.txt')) # создаем обьект класса потоки, где таргет функция необходиая к выполнению
+second = Thread(target=write_words,args = (30,'example6.txt'))
+third = Thread(target=write_words,args = (200,'example7.txt'))
+four = Thread(target=write_words,args = (100,'example8.txt'))
 
 
 first.start()  # старт потока относительно обьекта класса
-first.join()  # останавливает работу программы после завершения работы потока
 second.start()
-second.join()
 third.start()
-third.join()
 four.start()
+
+first.join()  # останавливает работу программы после завершения работы потока
+second.join()
+third.join()
 four.join()
 
 time_end2 = datetime.now()
